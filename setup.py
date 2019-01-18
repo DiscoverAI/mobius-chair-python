4cee40d811e3b26b776a7c203bb357b311ae6e88
Falco Winkler <falco.winkler@esailors.de>
2019-01-18 15:33:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="mobius_chair",
-    version="0.0.1",
-    author="Falco Winkler, Daniel Schruhl",
-    author_email="falcowinkler@icloud.com",
-    description="Library for managing versions of datasets or models.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DiscoverAI/mobius-chair-python",
+    name='mobius_chair',
+    version='0.0.1',
+    author='Falco Winkler, Daniel Schruhl',
+    author_email='falcowinkler@icloud.com',
+    description='Library for managing versions of datasets or models.',
+    long_description='''Version your datasets and models on any filesystem.
+    Refer to github for documentation: https://github.com/DiscoverAI/mobius-chair-python''',
+    url='https://github.com/DiscoverAI/mobius-chair-python',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: APACHE 2.0 License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers'
     ],
 )
